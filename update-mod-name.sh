6c48bd34512ee7b6b0b7b158fbb27856e3524172
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-09-23 17:40:44 +0100

    add script to convert to module

---
+++ update-mod-name.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -e
+
+# add a go mod file (auto migrates from dep)
+go mod init github.com/kava-labs/binance-chain-ledger-cosmos-go
+
+# Normally would need to up update self-imports, but there aren't any.
+
+# move tendermint imports to kava-labs/tendermint
+grep -rl --include=*.go \"github.com/tendermint/tendermint . | xargs sed -i '' 's#"github.com/tendermint/tendermint#"github.com/kava-labs/tendermint#'
+go mod edit --droprequire "github.com/tendermint/tendermint" -require "github.com/kava-labs/tendermint@v0.32.3-kava"
+go mod tidy
+
+# remove no longer needed dep files
+rm Gopkg.lock Gopkg.toml
