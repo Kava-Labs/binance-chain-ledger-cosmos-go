fcb6ad0e68d8b4bac8d187ab45fab3c5eb9ed4fc
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-09-23 19:25:43 +0100

    update to properly tagged tendermint fork

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/btcsuite/btcd v0.0.0-20190115013929-ed77733ec07d
 	github.com/go-logfmt/logfmt v0.4.0 // indirect
-	github.com/kava-labs/tendermint v0.32.3-kava
+	github.com/kava-labs/tendermint v0.32.3-kava1
 	github.com/stretchr/testify v1.3.0
 	github.com/zondax/hid v0.9.0 // indirect
 	github.com/zondax/ledger-go v0.9.0
--- update-mod-name.sh
+++ update-mod-name.sh
@@ -8,7 +8,7 @@ go mod init github.com/kava-labs/binance-chain-ledger-cosmos-go
 
 # move tendermint imports to kava-labs/tendermint
 grep -rl --include=*.go \"github.com/tendermint/tendermint . | xargs sed -i '' 's#"github.com/tendermint/tendermint#"github.com/kava-labs/tendermint#'
-go mod edit --droprequire "github.com/tendermint/tendermint" -require "github.com/kava-labs/tendermint@v0.32.3-kava"
+go mod edit --droprequire "github.com/tendermint/tendermint" -require "github.com/kava-labs/tendermint@v0.32.3-kava1"
 go mod tidy
 
 # remove no longer needed dep files
